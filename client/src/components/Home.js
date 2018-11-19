d68af4ef72c11635594efcbfdb9eb379ea5cdb5d
tchoutdara <monkeymilitia444@gmail.com>
2018-11-19 09:12:40 -0700

    text change/ideas

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import { 
   Header,
   Container,
+  Divider,
  } from 'semantic-ui-react';
 
 class Home extends Component {
   render() {
     return (
       <Container>
-        <Header as="h1" textAlign="center" style={{marginTop:"25px"}}>Hello! My name is Tony Choutdara and I am a Web Developer!</Header>
-        <Header as="h3" textAlign="center">Ruby | JavaScript | PostgreSQL</Header>
+        <Header as="h1" textAlign="center" style={{marginTop:"50px"}}>Hello! My name is Tony Choutdara and I am a Full Stack Web Developer!</Header>
+        <Divider />
+        <Header as="h3" textAlign="center">
+          Skill set section
+        </Header>
       </Container>
     );
   }
