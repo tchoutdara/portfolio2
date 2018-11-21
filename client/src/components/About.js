255bce7227e202a7d69b11c70999d5c3c586bfa1
tchoutdara <monkeymilitia444@gmail.com>
2018-11-21 09:29:32 -0700

    add text to about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -12,6 +12,10 @@ class About extends Component {
         <Header as="h1" textAlign="center" style={{marginTop:"25px"}}>A little bit about me...</Header>
           <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>About Myself</Header>
+          <p align="center">
+            Hi all I am a highly motivated, detail oriented, and hard working individual. I enjoy 
+            trying to figure out complex porblems and using my creativity when the opportunity arises.          
+          </p>
           <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Hobbies</Header>
       </Container>
