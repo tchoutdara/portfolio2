fede26a97d4a9dda57510d97881198a8e0ab293c
tchoutdara <monkeymilitia444@gmail.com>
2018-12-03 15:12:10 -0700

    adding images to home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,17 +6,11 @@ import {
   Image,
  } from 'semantic-ui-react';
  import Selfie from '../images/tchout.png';
- 
+ import Railslogo from '../images/railslogo.jpg'
+ import ReactLogo from '../images/reactlogo.png'
+
 
 class Home extends Component {
-
-  stackImage = () => {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
 
   render() {
     return (
@@ -25,11 +19,12 @@ class Home extends Component {
           <Header as="h1" textAlign="center" style={{marginTop:"10px"}}>
             Hello! My name is Tony Choutdara and I am a Full Stack Web Developer!
           </Header>
-
             <Divider />
           <Header as="h3" textAlign="center">
             Skill set section(ADD images of languages and then list out skills underneath)
           </Header>
+          <Image src={Railslogo} style={{height:"200px", width:"200px", marginTop:"10px"}} />
+          <Image src={ReactLogo} style={{height:"200px", width:"200px", marginTop:"10px"}} />
       </Container>
     );
   }
