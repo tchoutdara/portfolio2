a73b405e14569e85fb61ca638bbaf52986c1feab
tchoutdara <monkeymilitia444@gmail.com>
2018-12-10 12:31:35 -0700

    project page update

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -11,8 +11,6 @@ class ProjectPage extends Component {
       <Container>
         <Header as="h1" textAlign="center" style={{marginTop:"25px"}}>Projects Page</Header>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Projects</Header>
-          <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>To-Dont List(Screen Shot)</Header>
           <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 Web App(Screen Shot)</Header>
