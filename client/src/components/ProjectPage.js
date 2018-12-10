d1b9b2b09e16a3f915d17f96c886091e5f8fdad7
tchoutdara <monkeymilitia444@gmail.com>
2018-12-10 14:31:31 -0700

    cleaning up project page

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -8,6 +8,7 @@ import {
  import BO4 from '../images/BO4.png'
  import MDB from '../images/MovieDB.png'
  import TDList from '../images/TodontList.png'
+ import BO4Native from '../images/BO4Native.png'
 
 class ProjectPage extends Component {
   render() {
@@ -15,23 +16,29 @@ class ProjectPage extends Component {
       <Container>
         <Header as="h1" textAlign="center" style={{marginTop:"25px"}}>Projects Page</Header>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>To-Dont List(Screen Shot)</Header>
-        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px"}}>
+        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>To-Dont List</Header>
+        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px", width:"700px"}}>
             <Image src={TDList} style={{height:"200px", width:"300px", marginRight:"20px"}} />
             <p>A to do list with Google map integration.</p>
         </Container>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 Web App(Screen Shot)</Header>
-        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px"}}>
+        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 Web App</Header>
+        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px", width:"700px"}}>
             <Image src={BO4} style={{height:"200px", width:"300px", marginRight:"20px"}} />
             <p>Stat tracker for Call of Duty Black OPs 4</p>
         </Container>
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 React Native App(Screen Shot)</Header>
+        <Divider />
+        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 React Native App</Header>
+        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px", width:"700px"}}>
+            <Image src={BO4Native} style={{height:"300px", width:"150px", marginRight:"20px"}} />
+            <p>Stat tracker for Call of Duty Black OPs 4 on a mobile devise.</p>
+        </Container>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Movie Data Base App(Screen Shot)</Header>
-        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px"}}>
+        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Movie Data Base App</Header>
+        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px", width:"700px"}}>
             <Image src={MDB} style={{height:"200px", width:"300px", marginRight:"20px"}} />
-            <p>Web Application using The Movie Database API to pull up information for Movies, TV Shows, and People.</p>
+            <p>Web Application using The Movie Database API to 
+              pull up information for Movies, TV Shows, and People.</p>
         </Container>
       </Container>
     );
