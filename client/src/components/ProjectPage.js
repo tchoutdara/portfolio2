b5fd404f47a3629bc0552fa5ba9b4a3b5a99923c
tchoutdara <monkeymilitia444@gmail.com>
2018-12-10 13:12:55 -0700

    add bo4 screenshot

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -3,7 +3,10 @@ import {
   Header,
   Container,
   Divider,
+  Image,
  } from 'semantic-ui-react';
+ import BO4 from '../images/BO4.png'
+ import MDB from '../images/MovieDB.png'
 
 class ProjectPage extends Component {
   render() {
@@ -14,6 +17,10 @@ class ProjectPage extends Component {
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>To-Dont List(Screen Shot)</Header>
           <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 Web App(Screen Shot)</Header>
+        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px"}}>
+            <Image src={BO4} style={{height:"200px", width:"300px", marginRight:"20px"}} />
+            <p>Stat tracker for Call of Duty Black OPs 4</p>
+        </Container>
           <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 React Native App(Screen Shot)</Header>
           <Divider />
