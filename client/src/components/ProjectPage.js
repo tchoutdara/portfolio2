d529f2677386d515f937886913243e6177b247cf
tchoutdara <monkeymilitia444@gmail.com>
2018-12-10 14:23:26 -0700

    add screenshot for todont

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -7,6 +7,7 @@ import {
  } from 'semantic-ui-react';
  import BO4 from '../images/BO4.png'
  import MDB from '../images/MovieDB.png'
+ import TDList from '../images/TodontList.png'
 
 class ProjectPage extends Component {
   render() {
@@ -15,6 +16,10 @@ class ProjectPage extends Component {
         <Header as="h1" textAlign="center" style={{marginTop:"25px"}}>Projects Page</Header>
           <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>To-Dont List(Screen Shot)</Header>
+        <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px"}}>
+            <Image src={TDList} style={{height:"200px", width:"300px", marginRight:"20px"}} />
+            <p>A to do list with Google map integration.</p>
+        </Container>
           <Divider />
         <Header as="p" textAlign="center" style={{marginTop:"25px"}}>Call of Duty 4 Web App(Screen Shot)</Header>
         <Container style={{display:"flex", flexDirection:"row", justifyContent:"center", fontSize:"18px"}}>
