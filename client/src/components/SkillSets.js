831142a5de73862a32e03a8b44b75800d7e10b16
tchoutdara <monkeymilitia444@gmail.com>
2018-12-18 17:58:47 -0700

    import/add more logos to skill list

--- client/src/components/SkillSets.js
+++ client/src/components/SkillSets.js
@@ -14,6 +14,9 @@ import NativeLogo from '../images/reactnative.png'
 import JSLogo from '../images/JavaScript-logo.png'
 import RubyLogo from '../images/ruby.png'
 import HTMLLogo from '../images/html.png'
+import CSSLogo from '../images/css.png'
+import MySQLLogo from '../images/mysql.png'
+import RspecLogo from '../images/rspec.png'
 
 
 class SkillSets extends Component {
@@ -56,17 +59,26 @@ class SkillSets extends Component {
                   <Image src={HTMLLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
                   HTML
                 </List.Item> 
-                <List.Item>CSS</List.Item>
+                <List.Item>
+                  <Image src={CSSLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
+                  CSS
+                </List.Item>
               </List>
             </div>
             <div style={{margin:"20px"}}>
               <List>
-                <List.Item>MySQL</List.Item>
+                <List.Item>
+                  <Image src={MySQLLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
+                  MySQL
+                </List.Item>
                 <List.Item>
                   <Image src={PostgreSQLLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
                   PostgreSQL
                 </List.Item>
-                <List.Item>RSpec</List.Item>
+                <List.Item>
+                  <Image src={RspecLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
+                  RSpec
+                </List.Item>
                 <List.Item>GitHub</List.Item>
               </List>
             </div>
