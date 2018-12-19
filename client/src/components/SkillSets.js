a68c91abf3d09646e8afe7be96f571f1b529e243
tchoutdara <monkeymilitia444@gmail.com>
2018-12-18 17:53:45 -0700

    image imports

--- client/src/components/SkillSets.js
+++ client/src/components/SkillSets.js
@@ -12,6 +12,8 @@ import ReactLogo from '../images/reactlogo.png'
 import ReduxLogo from '../images/redux.png'
 import NativeLogo from '../images/reactnative.png'
 import JSLogo from '../images/JavaScript-logo.png'
+import RubyLogo from '../images/ruby.png'
+import HTMLLogo from '../images/html.png'
 
 
 class SkillSets extends Component {
@@ -42,16 +44,28 @@ class SkillSets extends Component {
             </div>
             <div style={{margin:"20px"}}>
               <List>
-                <List.Item>Ruby</List.Item>
-                <List.Item><Image src={Railslogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />Ruby on Rails</List.Item> 
-                <List.Item>HTML</List.Item> 
+                <List.Item>
+                  <Image src={RubyLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} /> 
+                  Ruby
+                </List.Item>
+                <List.Item>
+                  <Image src={Railslogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
+                  Ruby on Rails
+                </List.Item> 
+                <List.Item>
+                  <Image src={HTMLLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
+                  HTML
+                </List.Item> 
                 <List.Item>CSS</List.Item>
               </List>
             </div>
             <div style={{margin:"20px"}}>
               <List>
                 <List.Item>MySQL</List.Item>
-                <List.Item><Image src={PostgreSQLLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />PostgreSQL</List.Item>
+                <List.Item>
+                  <Image src={PostgreSQLLogo} style={{height:"20px", width:"20px", marginRight:"5px"}} />
+                  PostgreSQL
+                </List.Item>
                 <List.Item>RSpec</List.Item>
                 <List.Item>GitHub</List.Item>
               </List>
