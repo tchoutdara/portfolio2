2591ac514ed6625fd7d8ba62bf828ae0f000a005
tchoutdara <monkeymilitia444@gmail.com>
2018-12-28 15:39:42 -0700

    more theme changing

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -14,11 +14,11 @@ class ProjectPage extends Component {
   render() {
     return (
       <Container>
-        <Header as="h1" textAlign="center" style={{marginTop:"25px"}}>
+        <Header as="h1" textAlign="center" style={{marginTop:"25px", color:"white"}}>
           Projects Page
         </Header>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>
+        <Header as="p" textAlign="center" style={{marginTop:"25px", color:"white"}}>
           To-Dont List
         </Header>            
           <Image src={TDList} centered style={{height:"200px", width:"300px", marginBottom:"10px"}} />
@@ -26,7 +26,7 @@ class ProjectPage extends Component {
               A to do list with Google map integration.
             </Container>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>
+        <Header as="p" textAlign="center" style={{marginTop:"25px", color:"white"}}>
           Call of Duty 4 Web App
         </Header>
           <Image src={BO4} centered style={{height:"200px", width:"300px", marginBottom:"10px"}} />
@@ -34,7 +34,7 @@ class ProjectPage extends Component {
               Stat tracker for Call of Duty Black OPs 4
             </Container>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>
+        <Header as="p" textAlign="center" style={{marginTop:"25px", color:"white"}}>
           Call of Duty 4 React Native App
         </Header>
           <Image src={BO4Native} centered style={{height:"300px", width:"150px", marginBottom:"10px"}} />
@@ -42,7 +42,7 @@ class ProjectPage extends Component {
               Stat tracker for Call of Duty Black OPs 4 on a mobile devise.
             </Container>
           <Divider />
-        <Header as="p" textAlign="center" style={{marginTop:"25px"}}>
+        <Header as="p" textAlign="center" style={{marginTop:"25px", color:"white"}}>
           Movie Data Base App
         </Header>
           <Image src={MDB} centered style={{height:"200px", width:"300px", marginBottom:"20px"}} />
