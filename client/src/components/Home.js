4274e96a724b1a64b4081a2fb422f5df15d2d0a1
tchoutdara <monkeymilitia444@gmail.com>
2019-01-03 15:48:18 -0700

    color change

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -15,15 +15,15 @@ class Home extends Component {
     return (
       <Segment basic>
         <Image src={Selfie} circular centered style={{height:"300px", width:"250px", marginTop:"20px"}} />
-          <Header as="h1" textAlign="center" style={{marginTop:"20px", color:"white"}}>
+          <Header as="h1" textAlign="center" style={{marginTop:"20px", color:"#0086b3"}}>
             Hello! My name is Tony Choutdara and I am a Full Stack Web Developer!
           </Header>
           <SkillSets />  
           <Divider />  
-          <Header as="h1" textAlign="center" style={{marginTop:"20px", color:"white"}}>
+          <Header as="h1" textAlign="center" style={{marginTop:"20px", color:"#0086b3"}}>
             Resume 
           </Header>
-          <Segment basic textAlign="center">
+          <Segment basic textAlign="center" style={{color:"white"}}>
             Lay this section out similar to a resume with work experience, education, and professional summary.
           </Segment>
       </Segment>
